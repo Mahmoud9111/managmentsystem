beb2826e53fd7d26265ce760635e36b4e0b721ac
Mahmoud911 <mahmo@Mahmoud911>
2025-10-01 08:08:21 -0700

    streaming, suspenes

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,9 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function LoadingDasshboard(){
+    return(
+        <>
+            <Skeleton className="w-full h-[400px]"/>
+        </>
+    )
+}
